076dd7d55e1584823a7f7d09b2d1be4e70207148
Arthur Tham <19481191+arthurtham@users.noreply.github.com>
2021-10-13 15:23:53 -0700

    Implement basic objective 2 concepts mission

--- levels/oss_elephpant_2/objectives/objective2_concept/validator.js
+++ levels/oss_elephpant_2/objectives/objective2_concept/validator.js
@@ -1,33 +1,81 @@
-/*
-In your validation code, you can require core Node.js modules,
-third-party modules from npm, or your own code, just like a regular
-Node.js module (since that's what this is!)
-*/
 const assert = require("assert");
 const R = require("ramda");
 const { isTwilio } = require("../lib/example_helper");
 
-/*
-Objective validators export a single function, which is passed a helper
-object. The helper object contains information passed in from the game UI,
-such as what the player entered into the fields in the hack interface.
-
-The helper object also has "success" and "fail" callback functions - use
-these functions to let the game (and the player) know whether or not they 
-have completed the challenge as instructed.
-*/
 module.exports = async function (helper) {
   // We start by getting the user input from the helper
-  const { criticalIs1, criticalNot1, criticalNot2 } = helper.validationFields;
+  const { 
+    criticalNot1,
+    criticalNot2,
+    criticalNot3, 
+    criticalNot4,
+    criticalNot5,
+    criticalNot6,
+    criticalNot7,
+    criticalIs1,
+    criticalIs2,
+    criticalIs3,
+    criticalIs4,
+    criticalIs5,
+    criticalIs6,
+    criticalIs7
+   } = helper.validationFields;
 
-  // Next, you test the user input - fail fast if they get one of the
-  // answers wrong, or some aspect is wrong! Don't provide too much
-  // negative feedback at once, have the player iterate.
+  //All fields need to be answered
+  
+  if (
+    criticalNot1 === undefined ||
+    criticalNot2 === undefined ||
+    criticalNot3 === undefined ||
+    criticalNot4 === undefined ||
+    criticalNot5 === undefined ||
+    criticalNot6 === undefined ||
+    criticalNot7 === undefined ||
+    criticalIs1 === undefined ||
+    criticalIs2 === undefined ||
+    criticalIs2 === undefined ||
+    criticalIs3 === undefined ||
+    criticalIs4 === undefined ||
+    criticalIs5 === undefined ||
+    criticalIs6 === undefined ||
+    criticalIs7 === undefined 
+  ) {
+    return helper.fail(`Please answer all the questions!`);
+  }
+  
+  if (criticalIs1 === "false" || 
+  criticalIs2 === "false" || 
+  criticalIs3 === "false"|| 
+  criticalNot1 === "true" || 
+  criticalNot2 === "true") {
+      return helper.fail(`
+        Oops! Looks like you have a mistake there. 
+        Think about your answers about other people's thinking and try again!
+        `+criticalIs1+criticalIs2+criticalIs3+criticalNot1+criticalNot2+`
+        `);
+  }
 
-  // Validator 1: All question blocks must have some sort of answer in them
-  if (criticalIs1 || !criticalNot1 || !criticalNot2) {
+  if (
+    criticalIs4 === "false" ||
+    criticalIs5 === "false" ||
+    criticalNot6 === "true" ||
+    criticalNot7 === "true"
+     ) {
     return helper.fail(`
-      Critical Failure: `+criticalIs1+criticalNot1+criticalNot2+`
+      Oops! Looks like you have a mistake there. 
+      Think about your answers about your own thinking and try again!
+    `);
+  }
+
+  if (
+    criticalIs6  === "false" ||
+    criticalIs7  === "false" ||
+    criticalNot3  === "true" ||
+    criticalNot4  === "true" ||
+    criticalNot5  === "true" ) {
+    return helper.fail(`
+      Oops! Looks like you have a mistake there. 
+      Think about biases, contradictions and logic and try again!
     `);
   }
 
