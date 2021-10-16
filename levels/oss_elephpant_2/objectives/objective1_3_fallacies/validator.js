e6daf45859ec64fe0bfaf512bf2c486f3beea479
Arthur Tham <19481191+arthurtham@users.noreply.github.com>
2021-10-15 17:48:39 -0700

    Place fallacy stations in the objective 1.3 map and validate

--- levels/oss_elephpant_2/objectives/objective1_3_fallacies/validator.js
+++ levels/oss_elephpant_2/objectives/objective1_3_fallacies/validator.js
@@ -4,11 +4,24 @@ const R = require("ramda");
 const { isTwilio } = require("../lib/example_helper");
 
 module.exports = async function (helper) {
-  // Validator 1: All question blocks must have some sort of answer in them
+  const worldState = helper.world.getState("com.twilioquest.osselephpant2");
+  const fallacies = worldState.ossElephpant2.fallacies;
+  const {
+    answer1
+  } = helper.validationFields;
+
+  // Validator 1: Are enough stations solved?
+  if (!fallacies.canPass) {
+    return helper.fail(`
+    Please visit at least 3 fallacy stations before hacking this station.
+    `);
+  }
+
+
+  // Validator 2: All question blocks must have some sort of answer in them
   if (!answer1) {
     return helper.fail(`
-      Please answer all three questions before submitting your answer. Try scrolling
-      down on the questions panel if you don't see all the questions!
+      Please answer all questions before continuing!
     `);
   }
   helper.success(`
