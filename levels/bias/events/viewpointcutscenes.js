220ecc27da14f225918185037b901d21241d044e
Arthur Tham <19481191+arthurtham@users.noreply.github.com>
2021-11-10 12:42:26 -0800

    Modularized viewpoint cutscenes function for multiple cutscene support

--- levels/bias/events/viewpointcutscenes.js
+++ levels/bias/events/viewpointcutscenes.js
@@ -6,33 +6,42 @@ function viewpointEvent(world, worldState, event) {
         // If we're in the cryo room
         if (event.mapName === "cryo") {
             // TODO: criteria to introduce the player to the "LinkedIn NPCs"
-            if (true) {
-                world.forEachEntities("viewpoint", async (viewpoint) => {
-                    world.disablePlayerMovement();
-            
-                    await world.tweenCameraToPosition({
-                    x: viewpoint.startX,
-                    y: viewpoint.startY,
-                    });
-                    await world.wait(1500);
-            
-                    let chat = "objective2_2_schemaseeds_pre";
-                    worldState.Bias.conversations.ele.current = "none";
-                    processConversationEvents(event, world, worldState, chat);
-
-                    /*setupConversation(world, worldState, "ele_"+chat)
-                    worldState['Bias']['conversations']['ele'][chat]['current'] = "none";
-                    worldState['Bias']['conversations']['ele'][chat]['complete'] = true;
-                    worldState.Bias.conversations.ele.current = "none";*/
-            
-                    await world.tweenCameraToPlayer();
-            
-                    world.enablePlayerMovement();
-                });
+            if (!world.isObjectiveCompleted("objective2_2_unconsciousbiaspractice")) {
+                    viewpointEventHelper(world, worldState, event, "viewpoint", "none")
             }
         }
     }
+    // If the objective is closed:
+    else if (event.name == "objectiveDidClose") {
+        if (
+            (event.target.objectiveName === 'example_objective') && // Is objective 2.5.1 complete?
+            (true) // TODO: Is objective 2.5.2 deep maze not completed?
+        ) {
+            viewpointEventHelper(world, worldState, event, "viewpoint_2", "none")
+        }
+    }
+}
 
+function viewpointEventHelper(world, worldState, event, viewpointName, conversationName) {
+    world.forEachEntities(viewpointName, async (viewpoint) => {
+        world.disablePlayerMovement();
+
+        await world.tweenCameraToPosition({
+        x: viewpoint.startX,
+        y: viewpoint.startY,
+        });
+        await world.wait(1500);
+
+        if (conversationName !== "none") {
+            let chat = conversationName;
+            worldState.Bias.conversations.ele.current = "none";
+            processConversationEvents(event, world, worldState, chat);
+        }
+
+        await world.tweenCameraToPlayer();
+
+        world.enablePlayerMovement();
+    });
 }
 
 module.exports = {
